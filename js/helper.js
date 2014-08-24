5ff165fb59706a4237f660a57e844fd4fc1b2255
Pavel Handleman <pavel.handleman@gmail.com>
2014-08-24 23:54:01 +0300

    added inlineStyle function

--- js/helper.js
+++ js/helper.js
@@ -45,39 +45,52 @@ var HLP =(function(){
 			else {
 				var nsymbol = trg.charAt(trg.indexOf(current_browser[0])+current_browser[0].length);
 				if (trg.indexOf(current_browser[0]) > -1 && (nsymbol == " " || nsymbol == "")) return obj[key];
-			};
+			}
 		}
 		return none;
-	};
-	/*	how to use
-		
-		HLP.whatDevice.any() // returns true, if it is mobile browser
-		HLP.whatDevice.iOS() // returns true, if this is iPhone or iPad
+        },
+        /*	how to use
 
-		 you can use : .Android .BlackBerry .iOS .Opera .Windows .any
-	*/
-	var mobileDetection = {
-		Android:function () {
-			return navigator.userAgent.match(/Android/i) || false;
-		},
-		BlackBerry:function () {
-			return navigator.userAgent.match(/BlackBerry/i)  || false;
-		},
-		iOS:function () {
-			return navigator.userAgent.match(/iPhone|iPad|iPod/i)  || false;
-		},
-		Opera:function () {
-			return navigator.userAgent.match(/Opera Mini|Opera Mobi/i)  || false;
-		},
-		Windows:function () {
-			return navigator.userAgent.match(/IEMobile/i)  || false;
-		},
-		any:function () {
-			return (this.Android() || this.BlackBerry() || this.iOS() || this.Opera() || this.Windows());
-	}};
+            HLP.whatDevice.any() // returns true, if it is mobile browser
+            HLP.whatDevice.iOS() // returns true, if this is iPhone or iPad
 
+             you can use : .Android .BlackBerry .iOS .Opera .Windows .any
+        */
+        mobileDetection = {
+            Android:function () {
+                return navigator.userAgent.match(/Android/i) || false;
+            },
+            BlackBerry:function () {
+                return navigator.userAgent.match(/BlackBerry/i)  || false;
+            },
+            iOS:function () {
+                return navigator.userAgent.match(/iPhone|iPad|iPod/i)  || false;
+            },
+            Opera:function () {
+                return navigator.userAgent.match(/Opera Mini|Opera Mobi/i)  || false;
+            },
+            Windows:function () {
+                return navigator.userAgent.match(/IEMobile/i)  || false;
+            },
+            any:function () {
+                return (this.Android() || this.BlackBerry() || this.iOS() || this.Opera() || this.Windows());
+            }
+        },
+        inlineStyle = function(element, styleName){
+            var styles = element.getAttribute('style'),
+                value;
+            styles && styles.split(";").forEach(function (e) {
+                var style = e.split(":");
+                if (style[0].replace(/^\s+|\s+$/g, '') === styleName) {
+                    value = style[1];
+                }
+            });
+            return value;
+
+        };
 	return {
 		whatBrowser: browser,
-		whatDevice: mobileDetection
+		whatDevice: mobileDetection,
+        inlineStyle:inlineStyle
 	}
 }());
